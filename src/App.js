965a95fda01f4b72743ada84b4c6397595e6e1ac
Jack Anderson <whotfisjax@gmail.com>
2024-07-06 01:02:17 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -42,10 +42,10 @@ function App() {
       
       <div className="absolute top-10 right-10 flex flex-col items-center z-10">
         <div className="flex flex-row">
-          <a href="https://x.com/mikamikasol" className="p-2 hover:scale-110 transition ease-in-out duration-200">
+          <a href="https://x.com/syretoken" className="p-2 hover:scale-110 transition ease-in-out duration-200">
             <img src={Xlogo} alt="Xlogo" className="w-12 h-12 rounded-md" />
           </a>
-          <a href="https://t.me/mikamikasol" className="p-2 hover:scale-110 transition ease-in-out duration-200">
+          <a href="https://t.me/SYREONSOL" className="p-2 hover:scale-110 transition ease-in-out duration-200">
             <img src={TG} alt="Tg logo" className="w-12 h-12" />
           </a>
         </div>
